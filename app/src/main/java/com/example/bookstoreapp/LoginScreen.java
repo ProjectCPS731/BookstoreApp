6258592653a3d08ae488e69ab012b187be96cdca
Omid Hassan <fastomid@gmail.com>
2020-11-08 15:05:49 -0500

    Finished the Login Screen with the Firebase insertion and retrieval

--- app/src/main/java/com/example/bookstoreapp/LoginScreen.java
+++ app/src/main/java/com/example/bookstoreapp/LoginScreen.java
@@ -1,18 +1,31 @@
 package com.example.bookstoreapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 public class LoginScreen extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -31,22 +44,96 @@ public class LoginScreen extends AppCompatActivity {
         final EditText lastName2 = findViewById(R.id.edtLastName2);
         Button login = findViewById(R.id.btnLogin);
         Button create = findViewById(R.id.btnCreate);
-        Map<String, List<Object>> user = new HashMap<>();
+        final TextView test = findViewById(R.id.textView3);
+
+        final Map<String, String> user = new HashMap<>();
+        //user.put("0", "test test");
+        //db.collection("User's Names").document("Users").get().then();
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String FullName = firstName2.getText().toString().toLowerCase().trim() + " " + lastName2.getText().toString().toLowerCase().trim();
+                db.collection("User's Names").document("Users")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                DocumentSnapshot document = task.getResult();
+                                if (!document.exists())
+                                {
+                                    Toast.makeText(getApplicationContext(), "No one signed up yet. Be the first one sign up below", Toast.LENGTH_LONG);
+                                }
+                                Map<String, Object> tmp = document.getData();
+                                final boolean[] notfound = {true};
+                                for (Map.Entry<String, Object> entry : tmp.entrySet()) {
+                                   if (entry.getValue().equals(FullName))
+                                   {
+                                       notfound[0] = false;
+                                       Toast.makeText(getApplicationContext(), "Found your Account. Logging in...", Toast.LENGTH_SHORT);
+                                   }
 
+                                }
+                                if (notfound[0] == false)
+                                {
+                                    Toast.makeText(getApplicationContext(), "Credentials were not found. Create an account below.", Toast.LENGTH_LONG);
+                                }
+                            }
+                        });
             }
         });
 
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                List<Object> fullName = new ArrayList<>();
-                fullName.add(firstName2.getText());
-                fullName.add(lastName2.getText());
+                final String FullName = firstName2.getText().toString().toLowerCase().trim() + " " + lastName2.getText().toString().toLowerCase().trim();
+                final Random random = new Random();
+                final boolean[] notfound = {true};
+                final int[] randid2 = {random.nextInt((10000 - 1) + 1) + 1};
+                String t = "Users" + randid2;
+                db.collection("User's Names").document("Users")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                DocumentSnapshot document = task.getResult();
+                                if (!document.exists())
+                                {
+                                    user.put(randid2[0] + "", FullName);
+                                    db.collection("User's Names").document("Users").set(user, SetOptions.merge());
+                                }
+                                else
+                                {
+                                    Map<String, Object> tmp = document.getData();
+                                    for (int i = 0; i < 1; i++)
+                                    {
+                                        notfound[0] = true;
+                                        for (Map.Entry<String, Object> entry : tmp.entrySet()) {
+                                            if (Integer.parseInt(entry.getKey()) == randid2[0])
+                                            {
+                                                randid2[0] = random.nextInt((10000-1) + 1) + 1;
+                                                i=-1;
+                                                notfound[0] = false;
+                                                break;
+                                            }
+                                            else if (entry.getValue().equals(FullName))
+                                            {
+                                                notfound[0] = false;
+                                                // System.out.println(tmp.toString());
+                                                Toast.makeText(getApplicationContext(), "User already exists. Please login above", Toast.LENGTH_LONG).show();
+                                                break;
 
-
+                                            }
+                                        }
+                                    }
+                                    if (notfound[0] == true)
+                                    {
+                                        //System.out.println("Hi");
+                                        user.put(randid2[0] + "", FullName);
+                                        db.collection("User's Names").document("Users").set(user, SetOptions.merge());
+                                    }
+                                }
+                            }
+                        });
             }
         });
     }
--- app/src/main/java/com/example/bookstoreapp/NameOfUser.java
+++ app/src/main/java/com/example/bookstoreapp/NameOfUser.java
@@ -1,4 +1,79 @@
 package com.example.bookstoreapp;
 
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class NameOfUser {
+    private String FirstName;
+    private String LastName;
+    private String ID;
+
+    public NameOfUser(String FN, String LN, String id)
+    {
+        FirstName = FN;
+        LastName = LN;
+        ID = id;
+    }
+    public NameOfUser(String FN, String LN)
+    {
+        FirstName = FN;
+        LastName = LN;
+    }
+
+    public int checkIfUserExists(final String FullName)
+    {
+        final Map<String, String> user = new HashMap<>();
+
+        final Random random = new Random();
+        final boolean[] notfound = {true};
+        final int[] randid2 = {random.nextInt((10000 - 1) + 1) + 1};
+        FirebaseFirestore.getInstance().collection("User's Names").document("Users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot document = task.getResult();
+                        Map<String, Object> tmp = document.getData();
+                        System.out.println(tmp.toString());
+                        for (int i = 0; i < 1; i++)
+                        {
+                            notfound[0] = true;
+                            for (Map.Entry<String, Object> entry : tmp.entrySet()) {
+                                if (Integer.parseInt(entry.getKey()) == randid2[0])
+                                {
+                                    randid2[0] = random.nextInt((10000-1) + 1) + 1;
+                                    i=-1;
+                                    notfound[0] = false;
+                                    break;
+                                }
+                                else if (entry.getValue().equals(FullName))
+                                {
+                                    notfound[0] = false;
+                                    break;
+
+                                }
+                            }
+                        }
+
+                    }
+
+                });
+        if (notfound[0])
+        {
+            return randid2[0];
+        }
+        return 0;
+    }
 }
